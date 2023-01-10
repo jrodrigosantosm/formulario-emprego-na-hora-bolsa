3677b5d6377136a93cf723afdf8bf49f21c25619
Rodrigo Santos <105660050+jrodrigosantosm@users.noreply.github.com>
2023-01-10 16:09:55 -0300

    Adicionando sessionStore

--- scripts.js
+++ scripts.js
@@ -2,7 +2,15 @@ function redirecionar(){
     var candidato = document.getElementById("candidato").value
     alert(candidato+", seu pedido esta sendo processado, aguarde!");    
     window.location="conteplado.html";
+    const name = document.getElementById("candidato")
+    sessionStorage.setItem('name',name.value)    
 }
+    addEventListener("load",function(){
+    const info = sessionStorage.getItem('name')
+    let conteplado = document.getElementById("nomec")
+    conteplado.innerText ="Parabéns " + info   
+})
+
 
 
 

ffc133177b47f8e408b754b24c6fba378a89e4e7
Rodrigo Santos <105660050+jrodrigosantosm@users.noreply.github.com>
2023-01-10 17:36:00 -0300

    script

--- scripts.js
+++ scripts.js
@@ -1,12 +1,12 @@
 function redirecionar(){    
-    var candidato = document.getElementById("candidato").value
+    let candidato = document.getElementById("candidato").value
     alert(candidato+", seu pedido esta sendo processado, aguarde!");    
     window.location="conteplado.html";
-    const name = document.getElementById("candidato")
+    let name = document.getElementById("candidato")
     sessionStorage.setItem('name',name.value)    
 }
     addEventListener("load",function(){
-    const info = sessionStorage.getItem('name')
+    let info = sessionStorage.getItem('name')
     let conteplado = document.getElementById("nomec")
     conteplado.innerText ="Parabéns " + info   
 })
